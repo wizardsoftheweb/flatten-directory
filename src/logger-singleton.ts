bb3da0abe0a9749395c0def3c3672931868a8bb5
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-17 13:45:19 -0500

    Export the default transport's name

--- src/logger-singleton.ts
+++ src/logger-singleton.ts
@@ -1,13 +1,14 @@
 import { Logger, transports } from "winston";
 
 export const DEFAULT_LOG_LEVEL = "silly";
+export const DEFAULT_CONSOLE_TRANSPORT_NAME = "baseLogger";
 
 export const logger = new Logger({
     level: DEFAULT_LOG_LEVEL,
     transports: [
         new (transports.Console)({
             colorize: true,
-            name: "baseLogger",
+            name: DEFAULT_CONSOLE_TRANSPORT_NAME,
             timestamp: true,
         }),
     ],
