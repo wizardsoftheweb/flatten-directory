ce2c24e941a16385ecae0a4e0ffead0aec05cc65
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-17 13:45:32 -0500

    Add resetLoggerStub export

--- test/stubs/logger-singleton.ts
+++ test/stubs/logger-singleton.ts
@@ -1,6 +1,6 @@
 import { stub } from "sinon";
 
-export const loggerStub = {
+const loggerStub = {
     error: stub(),
     info: stub(),
     remove: stub(),
@@ -8,3 +8,13 @@ export const loggerStub = {
     verbose: stub(),
     warn: stub(),
 };
+
+function resetLoggerStub() {
+    for (const key in loggerStub) {
+        if (loggerStub.hasOwnProperty(key) && typeof (loggerStub as any)[key].reset !== "undefined") {
+            (loggerStub as any)[key].reset();
+        }
+    }
+}
+
+export { loggerStub, resetLoggerStub };
