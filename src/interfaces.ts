2c6f9ab7a52892d0a65b250df8c350deaebc6dcf
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-17 09:05:27 -0500

    Export IFlattenDirectoryOptions keys

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,15 @@
 import * as minimatch from "minimatch";
 import * as winston from "winston";
 
+
+/**
+ * Provides an array of the keys in `IFlattenDirectoryOptions`. TypeScript
+ * doesn't provide access to all the keys without some serious extra work.
+ * @type {Array}
+ * @see [StackOverflow](https://stackoverflow.com/questions/30207661/in-typescript-is-there-a-compile-time-way-to-get-all-the-property-names-defin)
+ */
+export const IFlattenDirectoryOptionsKeys = ["source", "target", "depth"];
+
 export interface IFlattenDirectoryOptions {
     source?: string;
     target?: string;
