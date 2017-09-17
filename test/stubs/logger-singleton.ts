4ecd33e2846372009df3534cccc3798fce5ac321
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-17 13:23:15 -0500

    Create a logger-singleton stub for proxyquire

---
+++ test/stubs/logger-singleton.ts
@@ -0,0 +1,10 @@
+import { stub } from "sinon";
+
+export const loggerStub = {
+    error: stub(),
+    info: stub(),
+    remove: stub(),
+    silly: stub(),
+    verbose: stub(),
+    warn: stub(),
+};
