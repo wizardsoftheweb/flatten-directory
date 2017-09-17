4a33482ce0304c2f1897d3e62511983931cb0b02
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-17 11:49:53 -0500

    Update parseOptions usage

--- src/flattenDirectory.ts
+++ src/flattenDirectory.ts
@@ -2,8 +2,8 @@ import * as Bluebird from "bluebird";
 import * as fs from "fs";
 import * as path from "path";
 
+import { parseOptions } from "./FlattenDirectoryOptions";
 import { IFlattenDirectoryOptions } from "./interfaces";
-import { parseOptions } from "./parseOptions";
 
 export function flattenDirectory(...args: any[]): Bluebird<any> {
     const options: IFlattenDirectoryOptions = parseOptions(args);
