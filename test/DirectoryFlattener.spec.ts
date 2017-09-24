9497db9b4394707d8b77a5e75d11c0357b9e4f83
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-24 00:20:50 -0500

    Document and test most of DirectoryFlattener

---
+++ test/DirectoryFlattener.spec.ts
@@ -0,0 +1,109 @@
+// Things like ...be.true; or ...be.rejected; dont play nice with TSLint
+/* tslint:disable:no-unused-expression */
+import * as chai from "chai";
+// Needed for describe, it, etc.
+import { } from "mocha";
+import * as proxyquire from "proxyquire";
+import * as sinon from "sinon";
+import * as sinonChai from "sinon-chai";
+
+const should = chai.should();
+chai.use(sinonChai);
+
+import * as fs from "fs";
+// import * as path from "path";
+import * as winston from "winston";
+
+import {
+    loggerStub,
+    resetLoggerStub,
+} from "./stubs/logger-singleton";
+
+const baseLogger = "baseLogger";
+const resolveStub = sinon.stub();
+
+const optionsStub = sinon.stub();
+const walkerStub = sinon.stub();
+
+const dummyArgs = ["/path/to/source", "/path/to/target", -1];
+const options = {
+    maxdepth: ((dummyArgs[2] as number) - 10),
+    source: dummyArgs[0] + "/qqq",
+    target: dummyArgs[1] + "/qqq",
+};
+
+const DirectoryFlattener = proxyquire("../src/DirectoryFlattener", {
+    "./DirectoryFlattenerOptions": {
+        DirectoryFlattenerOptions: optionsStub.returns({ options }),
+    },
+    "./DirectoryWalker": {
+        DirectoryWalker: walkerStub,
+    },
+    "./logger-singleton": {
+        DEFAULT_CONSOLE_TRANSPORT_NAME: baseLogger,
+        logger: loggerStub,
+    },
+    /* tslint:disable-next-line:object-literal-key-quotes */
+    "path": {
+        "@noCallThru": true,
+        resolve: resolveStub,
+    },
+}).DirectoryFlattener;
+
+
+
+describe("DirectoryFlattener", (): void => {
+    let flattener: any;
+    let factoryStub: sinon.SinonStub;
+
+    const dummyFactoryOutput = "cool";
+
+    beforeEach((): void => {
+        optionsStub.reset();
+        walkerStub.reset();
+        factoryStub = sinon.stub(DirectoryFlattener.prototype as any, "copierFactory");
+        factoryStub.withArgs(options.target).returns(dummyFactoryOutput);
+        optionsStub.callsFake(() => {
+            return { options };
+        });
+        buildNewFlattener();
+    });
+
+    describe("constructor", (): void => {
+        it("should initialize the options parser", (): void => {
+            optionsStub.should.have.been.calledOnce;
+            optionsStub.should.have.been.calledWith(...dummyArgs);
+        });
+
+        it("should initialize the walker", (): void => {
+            walkerStub.should.have.been.calledOnce;
+            walkerStub.should.have.been.calledWith({
+                callback: dummyFactoryOutput,
+                logger: loggerStub,
+                maxdepth: options.maxdepth,
+                root: options.source,
+            });
+        });
+    });
+
+    describe("flatten", (): void => {
+        it("should call walk", (): PromiseLike<void> => {
+            const walk = sinon.stub();
+            walk.resolves();
+            (flattener as any).walker = { walk };
+            return flattener.flatten()
+                .then(() => {
+                    walk.should.have.been.calledOnce;
+                });
+        });
+    });
+
+    afterEach((): void => {
+        flattener = null as any;
+        factoryStub.restore();
+    });
+
+    function buildNewFlattener(): void {
+        flattener = new DirectoryFlattener(...dummyArgs);
+    }
+});
