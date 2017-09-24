5f745ac28dcba1d92ab0873f8180d36875468e19
CJ Harries <cj@wizardsoftheweb.pro>
2017-09-24 07:02:26 -0500

    Test and document flattenDirectory

--- src/flattenDirectory.ts
+++ src/flattenDirectory.ts
@@ -2,7 +2,17 @@ import * as Bluebird from "bluebird";
 import * as fs from "fs";
 import * as path from "path";
 
+import {DirectoryFlattener} from "./DirectoryFlattener";
+
+/**
+ * Functional wrapper for `DirectoryFlattener.flatten`. Creates a new instance
+ * and immediately calls `flatten`.
+ *
+ * @param  {any[]}             ...args
+ * The args/options to pass to `DirectoryFlattener`
+ * @return {PromiseLike<void>}
+ * Resolves once the directory is flattened
+ */
 export function flattenDirectory(...args: any[]): PromiseLike<void> {
-    // do nothing
-    return Bluebird.resolve();
+    return (new DirectoryFlattener(...args)).flatten();
 }
